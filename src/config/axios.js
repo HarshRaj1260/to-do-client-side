f82f953854ff4effce37fbc0e9d429e0f2a349a3
Harsh Raj <2019kuec2045@iiitkota.ac.in>
2023-01-03 21:07:46 +0530

    corrected port number and the path of main.jsx

--- src/config/axios.js
+++ src/config/axios.js
@@ -10,7 +10,7 @@ const axios = _axios.create({
     baseURL:
         import.meta.env.MODE != "development"
             ? "/api"
-            : "https://localhost/api",
+            : "https://localhost:3000/api",
     timeout: 3000,
     headers: {
         Accept: "application/json",

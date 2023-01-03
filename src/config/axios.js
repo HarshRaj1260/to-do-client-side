5ee61e54ac291e304645dd281d906939d6ed1952
Harsh Raj <2019kuec2045@iiitkota.ac.in>
2023-01-03 20:47:17 +0530

    reversed changes

--- src/config/axios.js
+++ src/config/axios.js
@@ -10,7 +10,7 @@ const axios = _axios.create({
     baseURL:
         import.meta.env.MODE != "development"
             ? "/api"
-            : "https://to-do-api.onrender.com/api",
+            : "https://localhost/api",
     timeout: 3000,
     headers: {
         Accept: "application/json",
